6ccb0049154a37a7d85a62c593480f39093c57ed
Thanawat0107 <qwwasszxx@gmail.com>
2024-11-11 16:04:01 +0700

    Create Reuseable Header 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,51 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { View, Image, StyleSheet } from 'react-native'
 
 export default function Header() {
   return (
-    <View
-      style={{
-        flexDirection: "row",
-        flex: 0.5,
-        backgroundColor: "orange",
-        justifyContent: "space-between",
-        alignItems: "center"
-      }}
-    >
-      <Text>Header</Text>
-      <Text>Header</Text>
+    <View style={styles.header}>
+      <View style={styles.appDrawerContainer}>
+        <Image
+          source={require("../assets/apps.png")}
+          style={styles.appDrawerIcon}
+        />
+      </View>
+      <View>
+        <Image
+          source={require("../assets/Ellipse2.png")}
+          style={styles.profileImage}
+        />
+      </View>
     </View>
+
   );
 }
+
+const styles = StyleSheet.create({
+    appDrawerContainer: {
+      backgroundColor: "white",
+      height: 44,
+      width: 44,
+      borderRadius: 22,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    appDrawerIcon: {
+      height: 30,
+      width: 30,
+    },
+    appBackIcon: {
+      height: 24,
+      width: 24,
+      marginLeft: 10,
+    },
+    profileImage: {
+      height: 44,
+      width: 44,
+    },
+    header: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+    },
+  });
+  

7c1ce4067f851280ec5f8d4ae17d44da519b49ba
Thanawat0107 <qwwasszxx@gmail.com>
2024-11-11 14:54:35 +0700

    React Native Vector Icons & Adding Icons to Bottom Tab Navigation

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
-import * as React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
-import { View, Text } from 'react-native';
+import * as React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { View, Text } from "react-native";
+import AntDesign from '@expo/vector-icons/AntDesign';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Home!</Text>
     </View>
   );
@@ -16,15 +19,52 @@ function HomeScreen() {
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="HOME" component={HomeScreen} />
-        <Tab.Screen name="REORDER" component={HomeScreen} />
-        <Tab.Screen name="CART" component={HomeScreen} />
-        <Tab.Screen name="ACCOUNT" component={HomeScreen} />
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: true,
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: "red",
+        }}
+      >
+        <Tab.Screen
+          name="HOME"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ size, focused, color }) => {
+              return <AntDesign name={"home"} size={size} color={color} />;
+            },
+          }}
+        />
+        <Tab.Screen
+          name="REORDER"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ size, focused, color }) => {
+              return <Ionicons name="reorder-three" size={size} color={color} />;
+            },
+          }}
+        />
+        <Tab.Screen
+          name="CART"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ size, focused, color }) => {
+              return <AntDesign name="shoppingcart" size={size} color={color} />;
+            },
+          }}
+        />
+        <Tab.Screen
+          name="ACCOUNT"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ size, focused, color }) => {
+              return <MaterialIcons name="account-circle" size={size} color={color} />;
+            },
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
-    
-  )
-}
+  );
+};
 
-export default App
+export default App;

b863d80de998f1c7ed80a0a5bd461486c5bc33e3
Thanawat0107 <qwwasszxx@gmail.com>
2024-11-11 14:21:42 +0700

    Navigation and Tab Navigation

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { View, Text } from 'react-native';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="HOME" component={HomeScreen} />
+        <Tab.Screen name="REORDER" component={HomeScreen} />
+        <Tab.Screen name="CART" component={HomeScreen} />
+        <Tab.Screen name="ACCOUNT" component={HomeScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App

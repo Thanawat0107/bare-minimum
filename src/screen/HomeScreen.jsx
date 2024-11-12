63164c1d767df18eec6ececeb14d32c464596f9a
Thanawat0107 <qwwasszxx@gmail.com>
2024-11-12 21:04:07 +0700

    Search Input Field along with search Image

--- src/screen/HomeScreen.jsx
+++ src/screen/HomeScreen.jsx
@@ -1,4 +1,4 @@
-import { Platform, StyleSheet, View } from "react-native";
+import { Platform, StyleSheet, View, Text, Image, TextInput } from "react-native";
 import React from "react";
 import { LinearGradient } from "expo-linear-gradient";
 import Header from "../components/Header";
@@ -12,6 +12,14 @@ const HomeScreen = () => {
         style={styles.background}
       />
       <Header />
+      <Text style={styles.headerText}>Match Your Style</Text>
+      <View style={styles.inputContainer}>
+        <Image
+          source={require("../assets/search.png")}
+          style={styles.searchIcon}
+        />
+        <TextInput placeholder="Search" style={styles.textInput} />
+      </View>
     </View>
   );
 };
@@ -27,6 +35,7 @@ const styles = StyleSheet.create({
     // justifyContent: "center",
     backgroundColor: "white",
     marginTop: Platform.OS == "android" ? 25 : 0,
+    padding: 20,
   },
   background: {
     position: "absolute",
@@ -34,5 +43,26 @@ const styles = StyleSheet.create({
     right: 0,
     top: 0,
     height: 300,
+  },
+  headerText: {
+    fontSize: 28,
+    color: "#000000",
+    marginVertical: 20,
+  },
+  inputContainer: {
+    width: "100%",
+    backgroundColor: "#FFFFFF",
+    height: 48,
+    borderRadius: 12,
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  searchIcon: {
+    height: 26,
+    width: 26,
+    marginHorizontal: 12,
+  },
+  textInput: {
+    fontSize: 18,
   },
 });

68439fe86493e475e3cd710f5f5cba7283eaea26
Thanawat Butseree <qwwasszxx@gmail.com>
2024-11-18 10:54:31 +0700

    Implementing Horizontal Scrollable Category with FlatList 2

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,59 +1,75 @@
+import React, { useState } from "react";
 import {
+  View,
   FlatList,
   StyleSheet,
   Text,
+  StatusBar,
   TouchableOpacity,
-  View,
+  Alert,
 } from "react-native";
-import React, { useState } from "react";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+
+const DATA = [
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
+  },
+  {
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Second Item",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
+  },
+];
 
 const Category = () => {
-  const [selected, setSelected] = useState("Trending Now");
-  const tags = ["Trending Now", "All", "New", "Fashion", "Mens"];
+  const [selected, setSeleted] = useState(null);
+
+  const Item = ({ item }) => {
+    const { id, title } = item;
+
+    return (
+      <View style={styles.item}>
+        <Text style={styles.title}>{title}</Text>
+        {id === selected && Alert.alert("Selected", `Item: ${title}`)}
+      </View>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <FlatList
-        horizontal
-        data={tags}
-        showsHorizontalScrollIndicator={false}
-        renderItem={({ item }) => {
-          return (
-            <TouchableOpacity onPress={() => setSelected(item)}>
-              <Text
-                style={[
-                  styles.tagText,
-                  item == selected ? styles.isSelected : null,
-                ]}
-              >
-                {item}
-              </Text>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={DATA}
+          renderItem={({ item }) => (
+            <TouchableOpacity onPress={() => setSeleted(item.id)}>
+              <Item item={item} />
             </TouchableOpacity>
-          );
-        }}
-        contentContainerStyle={styles.container}
-      />
-    </View>
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 };
 
-export default Category;
-
 const styles = StyleSheet.create({
-  tagText: {
-    fontSize: 16,
-    borderRadius: 10,
-    paddingHorizontal: 30,
-    paddingVertical: 10,
-    marginHorizontal: 10,
-    backgroundColor: "#DFDCDC",
-    color: "#938F8F",
-    fontWeight: "700",
-  },
-  isSelected: {
-    backgroundColor: "#E96E6E",
-    color: "#FFFFFF",
-  },
   container: {
-    marginVertical: 10,
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
   },
 });
+
+export default Category;

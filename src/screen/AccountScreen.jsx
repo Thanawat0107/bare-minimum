78293cc2993611eeb7b53a7b297e062ec513e0d6
KENG!sBackward <qwwasszxx2345@gmail.com>
2024-12-16 16:12:34 +0700

    แบบที่ 2 Formik & Yup – Installation & Formik & Yup - Configuring the Validation Schema

--- src/screen/AccountScreen.jsx
+++ src/screen/AccountScreen.jsx
@@ -1,18 +1,7 @@
-import { 
-  TextInput, 
-  Button, 
-  StyleSheet, 
-  Text, 
-  View, 
-  Alert, 
-  Keyboard, 
-  TouchableWithoutFeedback 
-} from 'react-native';
-import React from 'react';
+import { TextInput, Button, StyleSheet, Text, View } from "react-native";
+import React from "react";
 import * as yup from "yup";
-import { useForm, Controller } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { LinearGradient } from "expo-linear-gradient";
+import { Formik } from "formik";
 
 const schema = yup.object().shape({
   email: yup.string().email("Invalid email").required("Email is required"),
@@ -20,110 +9,40 @@ const schema = yup.object().shape({
 });
 
 const AccountScreen = () => {
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-  });
-
-  const onPressSend = (formData) => {
-    Alert.alert(JSON.stringify(formData));
-  };
-
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={styles.container}>
-        <LinearGradient
-          colors={["rgba(9, 112, 24, 0.8)", "transparent"]}
-          style={styles.background}
-        />
-        <View style={styles.signupContainer}>
-          <Controller
-            control={control}
-            rules={{
-              required: true,
-            }}
-            render={({ field: { onChange, value } }) => (
+    <View>
+      <View>
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          onSubmit={(values) => console.log(values)}
+        >
+          {({ handleChange, handleBlur, handleSubmit, values }) => (
+            <>
               <TextInput
-                style={styles.textInput}
-                value={value}
-                onChangeText={onChange}
-                placeholder="Email"
+                name="email"
+                placeholder="Email Address"
+                onChangeText={handleChange("email")}
+                onBlur={handleBlur("email")}
+                value={values.email}
+                keyboardType="email-address"
               />
-            )}
-            name="email"
-          />
-          {errors.email && (
-            <Text style={styles.errorText}>{errors.email.message}</Text>
-          )}
-          <Controller
-            control={control}
-            rules={{
-              required: true,
-            }}
-            render={({ field: { onChange, value } }) => (
               <TextInput
-                style={styles.textInput}
-                value={value}
-                onChangeText={onChange}
+                name="password"
                 placeholder="Password"
+                onChangeText={handleChange("password")}
+                onBlur={handleBlur("password")}
+                value={values.password}
                 secureTextEntry
               />
-            )}
-            name="password"
-          />
-          {errors.password && (
-            <Text style={styles.errorText}>{errors.password.message}</Text>
+              <Button onPress={handleSubmit} title="Submit" />
+            </>
           )}
-          <Button title="Submit" onPress={handleSubmit(onPressSend)} />
-        </View>
+        </Formik>
       </View>
-    </TouchableWithoutFeedback>
+    </View>
   );
 };
 
 export default AccountScreen;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-    padding: 20,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  background: {
-    position: "absolute",
-    left: 0,
-    right: 0,
-    top: 0,
-    height: 300,
-  },
-  textInput: {
-    height: 40,
-    width: "100%",
-    marginBottom: 10,
-    backgroundColor: "white",
-    borderColor: "gray",
-    borderWidth: StyleSheet.hairlineWidth,
-    borderRadius: 10,
-  },
-  errorText: {
-    fontSize: 14,
-    color: "red",
-  },
-  signupContainer: {
-    width: "80%",
-    backgroundColor: "white",
-    padding: 20,
-    elevation: 10,
-    backgroundColor: "#e9f0ea",
-    borderRadius: 10,
-  },
-});
+const styles = StyleSheet.create({});

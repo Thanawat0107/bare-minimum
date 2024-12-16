de6d628fc12723704a58119043b229810b8d91ac
KENG!sBackward <qwwasszxx2345@gmail.com>
2024-12-16 15:54:19 +0700

    React Hook Form & Yup – Css Style

--- src/screen/AccountScreen.jsx
+++ src/screen/AccountScreen.jsx
@@ -1,12 +1,22 @@
-import { TextInput, Button, StyleSheet, Text, View } from 'react-native';
+import { 
+  TextInput, 
+  Button, 
+  StyleSheet, 
+  Text, 
+  View, 
+  Alert, 
+  Keyboard, 
+  TouchableWithoutFeedback 
+} from 'react-native';
 import React from 'react';
 import * as yup from "yup";
-import { useForm, Controller  } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { LinearGradient } from "expo-linear-gradient";
 
 const schema = yup.object().shape({
   email: yup.string().email("Invalid email").required("Email is required"),
-  password: yup.string().min(6, "Password must be at least 6 characters"),
+  password: yup.string().min(2, "Password must be at least 2 characters"),
 });
 
 const AccountScreen = () => {
@@ -23,63 +33,97 @@ const AccountScreen = () => {
   });
 
   const onPressSend = (formData) => {
-    // Perform actions with the validated form data
+    Alert.alert(JSON.stringify(formData));
   };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.formGroup}>
-        {/* Email Input */}
-        <Controller
-          control={control}
-          render={({ field: { onChange, value } }) => (
-            <TextInput
-              style={[
-                styles.input,
-                errors.email && styles.inputError, // Add error style
-              ]}
-              value={value}
-              onChangeText={onChange}
-              placeholder="Enter your email"
-              keyboardType="email-address" // Add keyboard type
-              autoCapitalize="none" // Disable auto-capitalize
-            />
-          )}
-          name="email"
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <LinearGradient
+          colors={["rgba(9, 112, 24, 0.8)", "transparent"]}
+          style={styles.background}
         />
-        {errors.email && (
-          <Text style={styles.errorText}>{errors.email.message}</Text>
-        )}
-
-        {/* Password Input */}
-        <Controller
-          control={control}
-          render={({ field: { onChange, value } }) => (
-            <TextInput
-              style={[
-                styles.input,
-                errors.password && styles.inputError, // Add error style
-              ]}
-              value={value}
-              onChangeText={onChange}
-              placeholder="Enter your password"
-              secureTextEntry // For password masking
-            />
+        <View style={styles.signupContainer}>
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                style={styles.textInput}
+                value={value}
+                onChangeText={onChange}
+                placeholder="Email"
+              />
+            )}
+            name="email"
+          />
+          {errors.email && (
+            <Text style={styles.errorText}>{errors.email.message}</Text>
           )}
-          name="password"
-        />
-        {errors.password && (
-          <Text style={styles.errorText}>{errors.password.message}</Text>
-        )}
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                style={styles.textInput}
+                value={value}
+                onChangeText={onChange}
+                placeholder="Password"
+                secureTextEntry
+              />
+            )}
+            name="password"
+          />
+          {errors.password && (
+            <Text style={styles.errorText}>{errors.password.message}</Text>
+          )}
+          <Button title="Submit" onPress={handleSubmit(onPressSend)} />
+        </View>
       </View>
-
-      {/* Submit Button */}
-      <Button title="Submit" onPress={handleSubmit(onPressSend)} />
-    </View>
+    </TouchableWithoutFeedback>
   );
-}
+};
 
-export default AccountScreen
+export default AccountScreen;
 
-
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    padding: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  background: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    height: 300,
+  },
+  textInput: {
+    height: 40,
+    width: "100%",
+    marginBottom: 10,
+    backgroundColor: "white",
+    borderColor: "gray",
+    borderWidth: StyleSheet.hairlineWidth,
+    borderRadius: 10,
+  },
+  errorText: {
+    fontSize: 14,
+    color: "red",
+  },
+  signupContainer: {
+    width: "80%",
+    backgroundColor: "white",
+    padding: 20,
+    elevation: 10,
+    backgroundColor: "#e9f0ea",
+    borderRadius: 10,
+  },
+});
